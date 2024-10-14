5ece523f5385a5f9fb090a80333b1badb09cc287
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-13 22:53:08 -0400

    Added systems and python-poetry template

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "A very basic flake";
+  description = "A collection of Nix Flake Templates";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+  in {
+    templates = {
+      python-poetry = {
+        description = "Nix Flake Template for Python using Poetry";
+        path = ./python-poetry;
+      };
+    };
   };
 }
