15fb48e5347bd08e42b3a758cca925c4a4d8b5f8
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-14 11:36:48 -0400

    Added formatter

--- flake.nix
+++ flake.nix
@@ -5,11 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgs = forEachSystem (system: import nixpkgs {inherit system;});
   in {
+    formatter = forEachSystem (system: pkgs.${system}.alejandra);
+
     templates = {
       default = {
         description = "Nix Flake Template for Development";
