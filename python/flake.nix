e449ba18c764c3d38a49c26bbd2bcf3391df6b2f
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-13 20:51:28 -0400

    Added flake.nix using Poetry

--- python/flake.nix
+++ python/flake.nix
@@ -1,15 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "Nix Flake Template for Python using Poetry";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    poetry2nix,
+  }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forEachSystem (system: {
+      default = pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          python3
+          poetry
+        ];
+      };
+    });
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    apps = forEachSystem (system: let
+      inherit (poetry2nix.lib.mkPoetry2Nix {pkgs = pkgs.${system};}) mkPoetryApplication;
+      app = mkPoetryApplication {projectDir = ./.;};
+    in {
+      default = {
+        type = "app";
+        program = "${app}/bin/app";
+      };
+    });
   };
 }
