d409fbf2975796c0d40f3636057184ae1ff8fb4e
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-14 23:49:43 -0400

    Added Crane and Fenix

--- rust/flake.nix
+++ rust/flake.nix
@@ -3,11 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
     self,
     nixpkgs,
+    fenix,
+    crane,
   }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
