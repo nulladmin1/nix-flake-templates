9ca60f11987cda470da5cfa92a59d294280f9255
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-29 14:42:43 +0000

    Added gomod2nix

--- go/flake.nix
+++ go/flake.nix
@@ -1,13 +1,18 @@
 {
-  description = "Nix Flake Template for Development";
+  description = "Nix Flake Template for Go using GoMod2Nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    gomod2nix,
   }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
