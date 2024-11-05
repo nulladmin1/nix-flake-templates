5a82333f70380a28ecf23d78c8f06f3667df232f
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-04 23:09:11 -0500

    Add rust-fenix-naersk-cross with cross-compile

---
+++ rust-fenix-naersk-cross/flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Nix Flake Template for Rust using Fenix and Naersk";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    naersk,
+    fenix,
+    ...
+  }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          fenix.overlays.default
+        ];
+      });
+    rust-toolchain = forEachSystem (system: pkgs.${system}.fenix.complete);
+  in {
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    devShells = forEachSystem (system: {
+      default = pkgs.${system}.mkShell {
+        packages = with rust-toolchain.${system}; [
+          cargo
+          rustc
+          clippy
+          rustfmt
+        ];
+        RUST_SRC_PATH = "${rust-toolchain.${system}.rust-src}/lib/rustlib/src/rust/library";
+      };
+    });
+
+    packages = forEachSystem (system: {
+      default = (pkgs.${system}.callPackage naersk {
+        cargo = rust-toolchain.${system}.cargo;
+        rustc = rust-toolchain.${system}.rustc;
+      }).buildPackage {
+        src = ./.;
+      };
+    });
+
+    apps = forEachSystem (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/rust";
+      };
+    });
+  };
+}
