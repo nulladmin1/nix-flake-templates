43968f155e686635aee642772f5bd481dc27cae6
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-10 19:43:11 -0500

    Add working uv2nix

--- python-uv/flake.nix
+++ python-uv/flake.nix
@@ -1,42 +1,71 @@
 {
-  description = "Nix Flake Template for Python with builtin Nix Builders";
+  description = "Nix Flake Template for Python using uv2nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:adisbladis/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    pyproject-nix,
+    uv2nix,
+    ...
   }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+    lib = forEachSystem (system: pkgs.${system}.lib);
+
+    workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel"; # or sourcePreference = "sdist";
+    };
+
+    pyprojectOverrides = _final: _prev: {
+    };
+
+    pythonSet = forEachSystem(system:
+      (pkgs.${system}.callPackage pyproject-nix.build.packages {
+        python = pkgs.${system}.python312;
+      }).overrideScope
+        (lib.${system}.composeExtensions overlay pyprojectOverrides)
+      );
   in {
     formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     devShells = forEachSystem (system: {
-      default = pkgs.${system}.mkShell {
+      default = let
+        editableOverlay = workspace.mkEditablePyprojectOverlay {
+          root = "$REPO_ROOT";
+        };
+        editablePythonSet = pythonSet.${system}.overrideScope editableOverlay;
+
+        virtualenv = editablePythonSet.mkVirtualEnv "app" workspace.deps.all;
+      in pkgs.${system}.mkShell {
         packages = with pkgs.${system}; [
-          python311
-        ] ++ (with pkgs.${system}.python311Packages; [
-          setuptools
-          pip
-        ]);
+          uv
+          virtualenv
+        ];
+        shellHook = ''
+          unset PYTHONPATH
+          export REPO_ROOT=$(git rev-parse --show-toplevel)
+        '';
       };
     });
 
     packages = forEachSystem (system: {
-      default = pkgs.${system}.python311Packages.buildPythonPackage rec {
-        pname = "app";
-        version = "0.1.0";
-        src = ./.;
-        format = "pyproject";
-
-        nativeBuildInputs = with pkgs.${system}.python311Packages; [
-          setuptools
-        ];
-      };
+      default = pythonSet.${system}.mkVirtualEnv "app" workspace.deps.default;
     });
 
     apps = forEachSystem (system: {
