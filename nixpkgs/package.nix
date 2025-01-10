27d0fd3b54c1a8616fa049693cb1719d24a8de66
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-10 18:40:22 -0500

    Sample package.nix

---
+++ nixpkgs/package.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  stdenv,
+}:
+stdenv.mkDerivation {
+  pname = "";
+  version = "";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+  ];
+
+  buildInputs = [
+  ];
+
+  meta = {
+    description = "";
+    homepage = "";
+    license = lib.licenses;
+    maintainers = with lib.maintainers; [];
+  };
+}
