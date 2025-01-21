9b71ed9b86ae15c3e894bf0b3a6ea037ae8534fc
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-20 19:18:52 -0500

    Add initiail structure for cli app

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+pub mod cli;
+use cli::Cli;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::init()?;
+    cli.run()?;
+
+    Ok(())
 }
