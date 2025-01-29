3d4f302d1bdc2fb94f568c837a6df1c740081982
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-28 23:11:09 -0500

    Initialize Zig

---
+++ zig/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "project_name";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    pkgsFor = forEachSystem (system: import nixpkgs {inherit system;});
+  in {
+    formatter = forEachSystem (system: pkgsFor.${system}.alejandra);
+
+    devShells = forEachSystem (system: {
+      default = pkgsFor.${system}.mkShell {
+        packages = with pkgsFor.${system}; [
+          hello
+        ];
+      };
+    });
+
+    packages = forEachSystem (system: {
+      default = pkgsFor.${system}.hello;
+    });
+
+    apps = forEachSystem (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/hello";
+      };
+    });
+  };
+}
