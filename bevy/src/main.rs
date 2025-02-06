1befec9017782456b243eb71b7c3e02cbb39734e
nulladmin1 <shrey.deogade@protonmail.com>
2025-02-06 12:49:42 -0500

    Add sample Bevy code

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_systems(Update, hello_world).run();
+}
+
+fn hello_world() {
+    println!("Hello, World!");
 }
