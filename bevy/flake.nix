c9c07c2845dd42382ca0284c4ac4c0f68a29b311
nulladmin1 <shrey.deogade@protonmail.com>
2025-02-06 16:46:19 -0500

    Finish Bevy devshell

--- bevy/flake.nix
+++ bevy/flake.nix
@@ -28,36 +28,52 @@
         ];
       });
     rust-toolchain = forEachSystem (system: pkgsFor.${system}.fenix.stable);
+
+    bevy-runtime-deps = forEachSystem (system:
+      with pkgsFor.${system}; [
+        pkg-config
+        alsa-lib
+        vulkan-tools
+        vulkan-headers
+        vulkan-loader
+        vulkan-validation-layers
+        libudev-zero
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libxcb
+        libxkbcommon
+        wayland
+      ]);
+    bevy-build-deps = forEachSystem (system:
+      with pkgsFor.${system}; [
+        clang
+        lld
+      ]);
+    all-deps = forEachSystem (system: bevy-build-deps.${system} ++ bevy-runtime-deps.${system});
   in {
     formatter = forEachSystem (system: pkgsFor.${system}.alejandra);
 
     devShells = forEachSystem (system: {
       default = pkgsFor.${system}.mkShell {
-        packages = with rust-toolchain.${system}; [
-          cargo
-          rustc
-          clippy
-          rustfmt
-        ];
+        packages = with rust-toolchain.${system};
+          [
+            cargo
+            rustc
+            clippy
+            rustfmt
+          ]
+          ++ all-deps.${system};
+        shellHook = ''
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgsFor.${system}.lib.makeLibraryPath (with pkgsFor.${system}; [
+            libxkbcommon
+            vulkan-loader
+          ])}"
+        '';
         RUST_SRC_PATH = "${rust-toolchain.${system}.rust-src}/lib/rustlib/src/rust/library";
       };
     });
-
-    packages = forEachSystem (system: {
-      default =
-        (pkgsFor.${system}.callPackage naersk {
-          inherit (rust-toolchain.${system}) cargo rustc;
-        })
-        .buildPackage {
-          src = ./.;
-        };
-    });
-
-    apps = forEachSystem (system: {
-      default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/project_name";
-      };
-    });
+    # TODO Figure out packages and apps
   };
 }
